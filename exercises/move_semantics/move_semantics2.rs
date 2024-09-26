2f84233c713adbfc6c7ab680dbad220db30bdc9d
yck <2998770265@qq.com>
2024-09-26 22:04:54 +0800

    update

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -9,9 +9,10 @@
 
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec1 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0.clone());
+    fill_vec(&mut vec1);
+    let vec0 = &vec1;
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -20,12 +21,10 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec:&mut Vec<i32>){
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
 }
